696cfe67524198f5e275de8a32b42b576c71aae1
rohitkm1404 <rohitkm1404@gmail.com>
2026-08-04 09:41:24 +0530

    Update python-primer.py

--- python-primer.py
+++ python-primer.py
@@ -73,7 +73,7 @@ def sum_of_squares(n):
     total=0
     for i in range (1,n):
         total +=i**2
-        return total 
+    return total 
 
 n=int(input("Enter a positive integer: "))
 print(sum_of_squares(n))
