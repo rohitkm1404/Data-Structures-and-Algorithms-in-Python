4b839185d2350aa87f275d89f0fc22a9aafd5601
rohitkm1404 <rohitkm1404@gmail.com>
2026-08-06 20:16:11 +0530

    Update Object-Oriented-Programming.py

---
+++ Object-Oriented-Programming.py
@@ -0,0 +1,23 @@
+#exercise solutions for object-oriented programming
+"""--------------------------------------------------Reinforcement-------------------------------------------------"""
+
+"""
+R-2.1 
+Give three examples of life-critical software applications.
+"""
+
+#solution
+
+"""
+1. Air Traffic Control Systems:
+ These systems manage the safe and efficient movement of aircraft in the airspace and on the ground.
+Any failure in this software could lead to catastrophic accidents.
+
+2. Car Airbag Control Systems:
+ These systems are responsible for deploying airbags in the event of a collision.
+A malfunction could result in airbags not deploying during an accident, leading to severe injuries or fatalities.
+
+3.Truck Anti-lock Braking Systems (ABS):
+ These systems prevent the wheels from locking up during braking, allowing the driver to maintain steering control.
+
+"""
