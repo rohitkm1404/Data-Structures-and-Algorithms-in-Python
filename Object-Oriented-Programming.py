ec15d622ace9de29852ccedd4f88f45f3e372f45
rohitkm1404 <rohitkm1404@gmail.com>
2026-08-07 20:34:06 +0530

    Implement Flower class with attributes and methods
    Added a new exercise solution for a Flower class with methods for setting and retrieving attributes.

--- Object-Oriented-Programming.py
+++ Object-Oriented-Programming.py
@@ -52,3 +52,40 @@ Methods that it might encapsulate include:
 - get_text(): Retrieve the current text content.
 - set_text(): Set the text content.
 """
+
+"""
+R-2.4
+Write a Python class, Flower, that has three instance variables of type str,
+int, and float, that respectively represent the name of the flower, its num-
+ber of petals, and its price. Your class must include a constructor method
+that initializes each variable to an appropriate value, and your class should
+include methods for setting the value of each type, and retrieving the value
+of each type.
+"""
+
+#solution
+
+class Flower:
+    def __init__(self, name: str, num_petals: int, price: float):
+        self.name = name
+        self.num_petals = num_petals
+        self.price = price
+
+    def set_name(self, name: str):
+        self.name = name
+
+    def get_name(self) -> str:
+        return self.name
+
+    def set_num_petals(self, num_petals: int):
+        self.num_petals = num_petals
+
+    def get_num_petals(self) -> int:
+        return self.num_petals
+
+    def set_price(self, price: float):
+        self.price = price
+
+    def get_price(self) -> float:
+        return self.price
+
